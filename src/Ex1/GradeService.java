ae96f16578e16563ff1a4e3e67ecfcabe90a72ab
VoinaRares <voinarares14@gmail.com>
2024-10-12 16:25:51 +0300

    Added average and roundedGrades for GradeService

--- src/Ex1/GradeService.java
+++ src/Ex1/GradeService.java
@@ -4,6 +4,28 @@ import java.util.Arrays;
 
 public class GradeService {
     public int[] unsatisfactoryGrades(int[] grades) {
-        return Arrays.stream(grades).filter((grade) -> grade < 38).toArray();
+        return Arrays.stream(grades).filter((grade) -> grade <= 38).toArray();
     }
+
+    public double average(int[] grades) {
+
+        double result = (double) Arrays.stream(grades).sum() / grades.length;
+        return Math.round(result * 100) / 100.0;
+    }
+
+    private int roundedGrade(int grade) {
+        if(grade < 38) {
+            return grade;
+        }
+        if(grade % 5 >= 3){
+            return grade + (5 - grade % 5);
+        }
+        return grade;
+    }
+
+    public int[] roundedUpGrades(int[] grades) {
+        return Arrays.stream(grades).map(this::roundedGrade).toArray();
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import Ex1.GradeService;
 
+import java.util.Arrays;
+
 //import java.util.Arrays;
 
 public class Main {
@@ -7,12 +9,18 @@ public class Main {
         GradeService gradeService = new GradeService();
         int[] grades = new int[]{29, 37, 38, 41, 84, 67};
 
+        //Ex1
         for( int grade : gradeService.unsatisfactoryGrades(grades) ) {
             System.out.println(grade);
         }
 
 //        System.out.println(Arrays.toString(gradeService.unsatisfactoryGrades(grades)));
 
+        //Ex2
+        System.out.println(gradeService.average(grades));
 
+
+        //Ex3
+        System.out.println(Arrays.toString(gradeService.roundedUpGrades(grades)));
     }
 }
